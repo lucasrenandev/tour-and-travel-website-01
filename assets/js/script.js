d84cd32bcad4d40980f69e0c133a0e685a54c41a
lucasrenandns <lucasrenandns96@gmail.com>
2022-12-29 19:20:55 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 const headerNav = document.querySelector("header nav")
+const scrollArrow = document.querySelector(".scroll-arrow")
 
 window.addEventListener("scroll", function() {
     headerNav.classList.toggle("sticky", window.scrollY > 0)
 })
+
+scrollArrow.addEventListener("click", function() {
+    window.scrollTo(0, 0)
+})
+
+function hiddenScrollArrow() {
+    if(window.scrollY < 800) {
+        scrollArrow.style.display = "none"
+    }
+    else {
+        scrollArrow.style.display = "inline-flex"
+    }
+}
+window.addEventListener("scroll", hiddenScrollArrow)
+hiddenScrollArrow()   
